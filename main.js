c7f92a9ce22b5a42a4a8a98f550808169c1f4e5d
Jubayer <mdjobayerhossainkhan@gmail.com>
2022-03-03 17:02:16 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const showTime = document.querySelector(".time");
+
+function updateTime(){
+  var currentTime = new Date()
+  var hours = currentTime.getHours()
+  var minutes = currentTime.getMinutes()
+  if (minutes < 10){
+      minutes = "0" + minutes
+  }
+  var t_str = hours + ":" + minutes + " ";
+  if(hours > 11){
+      t_str += "PM";
+  } else {
+      t_str += "AM";
+  }
+  showTime.innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
+
+
